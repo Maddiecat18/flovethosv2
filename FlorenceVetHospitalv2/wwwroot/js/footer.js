42e0fa1b3d730603c7b8df3a8382f033e901fa09
Lori Fair <lorijfair@hotmail.com>
2018-12-02 15:39:06 -0500

    Initial commit

---
+++ FlorenceVetHospitalv2/wwwroot/js/footer.js
@@ -0,0 +1,6 @@
+﻿
+
+$(document).ready(function () {
+    var currentDay = (new Date().getDay() > 0 ? new Date().getDay() - 1 : 6 )
+    $('.office-hours li').eq(currentDay).addClass('today');
+});
---
+++ FlorenceVetHospitalv2/wwwroot/js/staff.js
@@ -0,0 +1,35 @@
+﻿//This is the JQuery/ Ajax call to retrieve staff information for the staff page
+
+
+(function ($) {
+    FloVet = window.FloVet || { 'version': '0.1' };
+    FloVet.staff = FloVet.staff || { 'version': '0.1' };
+
+    FloVet.staff.staffSetup = function () {
+        $.ajax({
+            type: "GET",
+            url: "../api/staffmembers/GetAll",
+            contentType: "application/json; charset=utf-8",
+            success: function (response) {
+                var staffItems = response;
+                var sStaff = "";
+
+                for (var j = 0; j < staffItems.length; j++) {
+                    sStaff += "<div class='staffBio'>" +
+                        "<h3 class='nameTitle'>" +
+                        staffItems[j].Name +
+                        "</h3>" +
+                        "<p>" +
+                        staffItems[j].Description +
+                        "</p>" +
+                        "</div>";
+                };
+
+                $('#staffPlaceHolder').html(sStaff);
+
+            },
+            error: function (xhr, status, error) {
+            }
+        });
+    };
+}(jQuery));
