dd1611c5f8e396efe8f4c1f1616a1a86dd3b4c2f
Lori Fair <lorijfair@hotmail.com>
2018-12-06 11:52:02 -0500

    moved footer.js jquery to the site.js that is in the bundleconfig.json file. Added items to "Our Services" section on the Pet Services page.

--- FlorenceVetHospitalv2/wwwroot/js/footer.js
+++ FlorenceVetHospitalv2/wwwroot/js/footer.js
@@ -1,6 +1,6 @@
 ﻿
 
-$(document).ready(function () {
-    var currentDay = (new Date().getDay() > 0 ? new Date().getDay() - 1 : 6 )
-    $('.office-hours li').eq(currentDay).addClass('today');
-});
+//$(document).ready(function () {
+//    var currentDay = (new Date().getDay() > 0 ? new Date().getDay() - 1 : 6 )
+//    $('.office-hours li').eq(currentDay).addClass('today');
+//});
