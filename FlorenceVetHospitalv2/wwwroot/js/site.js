655676a618fa02e356bc095a7736e5b19642f375
Lori Fair <lorijfair@hotmail.com>
2019-01-16 15:55:43 -0500

    added meta tags, small changes on about page and footer, deleted the staff.js file

--- FlorenceVetHospitalv2/wwwroot/js/site.js
+++ FlorenceVetHospitalv2/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿// Write your JavaScript code.
+﻿// this script highlights the current day's hours in the footer
 $(document).ready(function () {
     var currentDay = (new Date().getDay() > 0 ? new Date().getDay() - 1 : 6)
     $('.office-hours li').eq(currentDay).addClass('today');
