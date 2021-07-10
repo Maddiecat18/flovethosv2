060ff01ea3eed0dd6692bb53ff69932b8bf29107
Lori Fair <lorijfair@hotmail.com>
2021-07-10 16:07:50 -0400

    Edits to the About Us text per Dr. Dan Added the PetInformation.cs and .cshtml pages Added PetInfo.js and related script additions Added the Pet Information item to main navigation menu

---
+++ FlorenceVetHospitalv2/wwwroot/js/PetInfo.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $(".petInfodiv").hide();
+    $("#petInfoLinks a").click(function () {
+        var container = $(this).data("container");
+        $(".petInfodiv").hide();
+        $("#" + container).show();
+        $("#frmain").hide();
+
+    });
+
+});
